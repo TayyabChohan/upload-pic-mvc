be047bcae963e86c25054484ff8a3d242c65273f
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2019-02-11 12:50:20 +0500

    picture Upload

--- WebAppImage/Controllers/HomeController.cs
+++ WebAppImage/Controllers/HomeController.cs
@@ -3,27 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppImage.Models;
 
 namespace WebAppImage.Controllers
 {
     public class HomeController : Controller
     {
+        imageListEntities db = new imageListEntities();
         public ActionResult Index()
         {
-            return View();
+            var list = db.mYimages.ToList(); 
+            return View(list);
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Create(int id=0)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            mYimage tb = new mYimage();
+            if (id > 0)
+            {
+                tb = db.mYimages.Find(id);
+                
+            }
+            TempData["tmp"] = tb.Image;
+            return View(tb);
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Create(mYimage m, HttpPostedFileBase img)
         {
-            ViewBag.Message = "Your contact page.";
-
+            if (ModelState.IsValid)
+                if(img!=null)
+                {
+                    img.SaveAs(HttpContext.Server.MapPath("~/Uploads/") + img.FileName);
+                    m.Image = img.FileName;
+                }
+            else if(img==null)
+                {
+                    if (TempData["tmp"] == null)
+                    {
+                        m.Image = "default.jpg";
+                    }
+                    else
+                        m.Image = TempData["tmp"].ToString();
+                }
+            if(m.imageId>0)
+            {
+                db.Entry(m).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                db.mYimages.Add(m);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
