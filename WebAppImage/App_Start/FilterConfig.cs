765c6de8c9a7b996511f055d902d66b69203b744
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2019-02-11 11:41:27 +0500

    Add project files.

---
+++ WebAppImage/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppImage
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
